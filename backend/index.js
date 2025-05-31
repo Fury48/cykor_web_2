9d74b10c5b345d7c3b7005ca92ca09ca97fa451b
Fury48 <gamingfury48@gmail.com>
2025-05-31 14:32:46 +0900

    db 연결

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
+// app.listen(5001,()=>{
+//     console.log("");
+// })
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.listen(5001,()=>{
-    console.log("");
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('DB 연결 성공'))
+    .catch(err => console.error( err));
+
+const Users = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    pw: {
+        type: String,
+        required: true
+    }
+});
--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import './App.css'
+import Login from "./login";
+import Signin from "./signin";
 
 function App() {
   // useState 변수 사용하는 자리 ex) const [count, setCount] = useState(0)
